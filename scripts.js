efb55f9b2262c87195fb563bda934a3dc6b016b4
Robert Nevins <renevins@earthlink.net>
2019-01-25 15:44:10 -0500

    Added console output

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 var txt
-var str = prompt("Please enter your name:", "Bugs Bunny");
+var str = "Bugs Bunny";
 var pos = str.indexOf(" ");
 var fname = str.slice(0,pos);
 var lname = str.slice(pos+1,);
@@ -7,4 +7,8 @@ var lname = str.slice(pos+1,);
 //txt = pos + '\n' + fname + "\n" + lname;
 //alert(txt);
 
-document.getElementById("demo").innerHTML = pos + "<br />" + "First name = " + fname + "<br />" + "Last name = " +lname;
+//document.getElementById("demo").innerHTML = pos + "<br />" + "First name = " + fname + "<br />" + "Last name = " +lname;
+
+console.log(fname);
+
+
